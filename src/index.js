ed3dc7066cf18aa1352d37ceda1178d600aeabee
Gabriel Caetano <gvcaetano@inf.ufsm.br>
2022-02-13 19:21:08 -0300

    fix: typo in findIndex method

--- src/index.js
+++ src/index.js
@@ -30,14 +30,12 @@ app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  const repositoryIndex = repositories.findindex(repository => repository.id === id);
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
-
   const repository = { ...repositories[repositoryIndex], ...updatedRepository };
-
   repositories[repositoryIndex] = repository;
 
   return response.json(repository);
